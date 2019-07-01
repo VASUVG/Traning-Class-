c9c8d43df279a3e094798957e6a99e2106a846c1
VASUVG <52270221+VASUVG@users.noreply.github.com>
2019-07-01 11:01:30 +0530

    Create Alphabet.py

---
+++ Alphabet.py
@@ -0,0 +1,10 @@
+c = input("")
+if(c  == 'a' or c == 'e' or c == 'i' or c== 'o' or c == 'u' or c== 'A'
+       or c == 'E' or c == 'I' or c == 'O' or c == 'U'or c == 'b' or c == 'c' or c == 'd' or c == 'f' or c == 'g' or c== 'h'
+       or c == 'j' or c == 'k' or c == 'l' or c == 'm'or c == 'n' or c == 'p' or c == 'q' or c == 'r' or c == 's' or c == 't'
+       or c == 'v' or c == 'w' or c == 'x' or c == 'y'or c=='z'or c == 'B' or c == 'C' or c == 'D' or c == 'F' or c == 'G' or c == 'H'
+       or c == 'J' or c == 'K' or c == 'L' or c == 'M'or c == 'N' or c == 'P' or c == 'Q' or c == 'R' or c == 'S' or c == 'T'
+       or c == 'V' or c == 'W' or c == 'X' or c == 'Y'or c=='Z' ): 
+    print("Alphabet")
+else:
+     print("No") 
