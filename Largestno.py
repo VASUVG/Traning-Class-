b86201e7696dfa970de1be4730ae4e4ebabed2ec
VASUVG <52270221+VASUVG@users.noreply.github.com>
2019-07-01 11:14:08 +0530

    Update Largestno.py

--- Largestno.py
+++ Largestno.py
@@ -1,9 +1,11 @@
-a,b,c=map(int,(input().split()))
-if (a > b and a > c):
-          print(a)
-elif (b > a and b > c):
-          print(b)
-elif (c > a and c > b):
-          print(c)
+print("")
+ 
+a = int(input())
+b = int(input())
+c = int(input())
+if a>b and a>c:
+	print("largest")
+elif b>a and b>c:
+	print("largest")
 else:
-          print(a)
+	print("largest")
