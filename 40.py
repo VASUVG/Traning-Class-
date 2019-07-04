fb33bbb47aa3cc615306baa2b63db9c29c8eccc8
VASUVG <52270221+VASUVG@users.noreply.github.com>
2019-07-04 11:22:55 +0530

    Update 40.py

--- 40.py
+++ 40.py
@@ -1,7 +1,7 @@
-cat=int(input())
-sm1,sm2=0,1
-print(sm2,end=" ")
-for i in range(1,cat):
- sm3=sm1+sm2
- print(sm3,end=" ")
-sm1,sm2=sm2,sm3
+vsm=int(input())
+vas,deva=0,1
+print(deva,end=" ")
+for i in range(1,vsm):
+  faboot=vas+deva
+  print(faboot,end=" ")
+vas,deva=devat,faboot
