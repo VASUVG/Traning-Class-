15b7c20c680a0095114044dabb530540cee64a0e
VASUVG <52270221+VASUVG@users.noreply.github.com>
2019-08-07 15:17:15 +0530

    Create pro18.py

---
+++ pro18.py
@@ -0,0 +1,16 @@
+vv,ss=map(int,input().split())
+tem=[]
+z=0
+for n in range(vv):
+    tem.append(list(map(int,input().split())))   
+for n in range(vv):
+    for i in range(ss-1):
+        for r in range(i+1,ss+1):
+            if tem[n][i:r]==[1]*len(tem[n][i:r]):
+                 if all(tem[q+n][i:r]==[1]*len(tem[q+n][i:r]) for q in range(len(tem[n][i:r])-1)):
+                     if len(tem[n][i:r])>z:
+                        x=len(tem[n][i:k])
+if len(tem)==1 and len(tem[0])==1 and tem[0][0]==1:
+    print(1)
+for n in range(z):
+    print(*[1]*z)
